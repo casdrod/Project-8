7cf901aef3195664bed1b1cc05667084282fa2e4
Casey Rodriguez <casdrod@gmail.com>
2020-02-19 12:29:20 -0600

    Employee Containers complete; working on modal

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
-// let employees = [];
-// const modal = document.querySelector('modal');
-// const employeeContainer = document.querySelectorAll('employee-container');
-// const modalContent = document.querySelector('modal-content');
-// let mainContainer = document.querySelector('main-container')
+let employees = [];
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const gridContainer = document.querySelector('.grid-container');
+const mainContainer = document.querySelector('.main-container');
+
 
 // FETCHING RANDOM USERS //
 fetch('https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture')
     .then(response => response.json())
     .then(data => generateEmployees(data.results))
-    .catch(err => document.getElementById('employees')
-        .innerHTML = 'There was an error while trying to retreive this data.'
-        .fontcolor('red'))
+    .catch(err => console.log(err))
 
 
+
+//************************//
 //****HELPER FUNCTIONS****//
-
+//************************//
 // Function to create employee boxes //
 function generateEmployees(data) {
     const employees = data;
-    var statusHTML = '<div class="grid-container">';
-    employees.forEach(employee => {
+    var statusHTML = '';
+    employees.forEach((employee, index) => {
         statusHTML += `
-            <div class="employee-container">
+            <div class="employee-container" data-index="${index}">
                 <img src="${employee.picture['large']}" alt="employee's picture">
                 <ul>
                     <li><h2 class="employee_name">${employee.name['first']}${employee.name['last']}</h2></li>
@@ -31,40 +32,53 @@ function generateEmployees(data) {
             </div>
             `;
     });
-    statusHTML += '</div>';
-    document.getElementsByClassName('main-container').innerHTML = statusHTML;
+    gridContainer.innerHTML = statusHTML;
+}
+
+// Function to create popup boxes //
+function generateModal(index) {
+    let { name,
+        email,
+        location: {street, city, state, postcode},
+        cell,
+        dob,
+        picture } = employees[index];
+
+    let date = new Date(dob.date);
+
+    var modalHTML = `
+        <div class="top-container">
+            <image src="${picture['large']}">
+            <ul>
+                <li><h2 class="employee_name">${name['first']} ${name['last']}</h2></li>
+                <li>${email}</li>
+                <li>${city}</li>
+            </ul>
+        </div>
+        <div class="bottom-container">
+            <ul>
+                <li>${cell}</li>
+                <li>${location}</li>
+                <li>Birthday:${date}</li>
+            </ul>
+        </div>
+    `;
+    
+    modalContent.innerHTML = modalHTML;
 }
 
 
+//****Event Listener****//
+gridContainer.addEventListener('click', e => { 
+    if (e.target !== gridContainer) {
+        const employee = e.target.closest(".employee-container");
+        const index = employee.getAttribute('data-index');
 
-// // Function to create popup boxes //
-// function generateModal(index) {
-//     let {picture, name, email, location: {street, city, state, postcode}, cell, dob} = employees[index];
+        generateModal(index);
+        modal.style.display = 'block'; 
+    }
+}); 
 
-//     let date = new Date(dob.date);
 
-//     var modalHTML = `
-//         <div class="top-container">
-//             <div class="employee-container">
-//             <image src="${picture['large']}">
-//             <ul>
-//             <li><h2 class="employee_name">${name['first']} ${name['last']}</h2></li>
-//             <li>${email}</li>
-//             <li>${city}</li>
-//             </div>
-//         </div>
-//         <div class="bottom-container">
-//             <ul>
-//                 <li>${cell}</li>
-//                 <li>${location}</li>
-//                 <li>Birthday:${date}</li>
-//         </div>
-//     `;
-//     modal.style.display = 'block';
-//     modalContent.innerHTML = modalHTML;
-// }
 
-// employeeContainer.addEventListener('click', () => {
-//     generateModal(index);
-// })
-
+// go back an simplify with variables
