8f3bb52549d584f2ac8b89e02135f6827ff57e0d
Casey Rodriguez <casdrod@gmail.com>
2020-02-27 16:19:56 -0600

    Next button on modal complete. Working on previous button.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,18 @@ const modalContainer = document.querySelector('.modal-container');
 const gridContainer = document.querySelector('.grid-container');
 const mainContainer = document.querySelector('.main-container');
 const userUrl = 'https://randomuser.me/api/?results=12&inc=name,location,email,dob,cell,picture&nat=US';
-let employeeArr = [];
+let employees = [];
 
 // FETCHING RANDOM USERS //
 fetch(userUrl)
     .then(response => response.json())
     .then(data => data.results)
     .then(data => {
-        employeeArr.push(data);
-        console.log(employeeArr[0]);
+        employees.push(data);
+        employees = employees[0];
+        generateEmployees(employees);
     })
     .catch(err => console.log(err))
-
-// console.log(employeeArr);
 
 
 //************************//
@@ -24,6 +23,7 @@ fetch(userUrl)
 // FUNCTION TO CREATE EMPLOYEE BOXES //
 function generateEmployees(data) {
     const employees = data;
+    console.log(employees.length);
     var statusHTML = '';
     employees.forEach((employee, index) => {
         let picture = employee.picture.large;
@@ -56,54 +56,68 @@ function generateEmployees(data) {
 
 // FUNCTION TO CREATE HTML FOR MODAL //
 function generateModal(employee) {
-    let picture = employee.picture.large;
-    let firstName = employee.name.first;
-    let lastName = employee.name.last;
-    let email = employee.email;
-    let cell = employee.cell;
-    let address = `${employee.location.street.number} 
-                    ${employee.location.street.name}<br>
-                    ${employee.location.city}, 
-                    ${employee.location.state} 
-                    ${employee.location.postcode}
-                    `;
-    let state = employee.location.street.state;
-    let dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language);
 
-    modalContainer.innerHTML = `
-        <div class="modal-box">
-            <span class="close">&times;</span>
-            <div class="modal-info">
-                <div class="arrow">
-                    <i class="fas fa-chevron-left prev-btn"></i>
-                </div>
-                <div class="modal-content">
-                    <image src="${picture}" alt="employee's picture" class="modal-picture">
-                    <h2 class="employee_name">${firstName} ${lastName}</h2>
-                    <p>${email}</p>
-                    <hr>
-                    <p>${cell}</p>
-                    <p>${address}</p>
-                    <p>Birthday: ${dob}</p>
-                </div>
-                <div class="arrow">
-                    <i class="fas fa-chevron-right next-btn"></i>
+    function modalHtml(employee) {
+        let dob = new Date(Date.parse(employee.dob.date)).toLocaleDateString(navigator.language);
+        modalContainer.innerHTML = `
+            <div class="modal-box">
+                <span class="close">&times;</span>
+                <div class="modal-info">
+                    <div class="arrow">
+                        <i class="fas fa-chevron-left prev-btn"></i>
+                    </div>
+                    <div class="modal-content">
+                        <image src="${employee.picture.large}" alt="employee's picture" class="modal-picture">
+                        <h2 class="employee_name">${employee.name.first} ${employee.name.last}</h2>
+                        <p>${employee.email}</p>
+                        <hr>
+                        <p>${employee.cell}</p>
+                        <p>${employee.location.street.number} 
+                            ${employee.location.street.name}<br>
+                            ${employee.location.city},<br>
+                            ${employee.location.state} 
+                            ${employee.location.postcode}</p>
+                        <p>Birthday: ${dob}</p>
+                    </div>
+                    <div class="arrow">
+                        <i class="fas fa-chevron-right next-btn"></i>
+                    </div>
                 </div>
             </div>
-        </div>
-    `;
-    
-    modalContainer.style.display = 'block';
+        `;
 
-    var i = 0;
+        // Event Listener for Next Button //
+        document.querySelector('.next-btn').addEventListener('click', () => {
+            nextItem(employee);
+        })
 
-    function nextItem() {
-        i = i + 1;
-        console.log(employeeArr[0]);
+        // Event Listener for Previous Button //
+        document.querySelector('.prev-btn').addEventListener('click', () => {
+            prevItem(employee);
+        })
     }
 
-    nextItem();
+    modalContainer.style.display = 'block';
+    modalHtml(employee);
 
+    // Function for Next Item Button //
+    var i = employees.indexOf(employee);
+    function nextItem(employee) {
+        i++;
+        if (i === employees.length) {
+            i = 0;
+        }
+        modalHtml(employees[i]);
+    }
+
+    function prevItem(employee) {
+        i = i - 1;
+        // if (i === 0) {
+        //     i = employees.length;
+        // }
+        modalHtml(employees[i]);
+        console.log(i);
+    }
 }
 
 
